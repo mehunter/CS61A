fa44511bf1eaf8b347f71809323c0b0c1485b9b1
Matt Hunter <thematthunter@gmail.com>
2019-08-11 17:15:30 -0600

    add README to initial commit

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_eighteen():
+    """Come up with the most creative expression that evaluates to 2018,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_eighteen()
+    2018
+    """
+    return 2**10 + 2**9 + 2**8 + 2**7 + 2**6 + 2**5 + 2
